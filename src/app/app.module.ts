c73a639c03d20bf8c4d6297c72b97a27c4d74dd9
Hung hq <hunghq@samsolutions.vn>
2019-01-30 17:16:12 +0700

    5:16pm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
+import { DataService } from './services/data.service';
 
 @NgModule({
   declarations: [
@@ -12,9 +15,11 @@ import { UserComponent } from './components/user/user.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-user',
@@ -6,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.sass']
 })
 export class UserComponent implements OnInit {
-  name:string;
-  age:number;
-  email:string;
+  name: string;
+  age: number;
+  email: string;
   // address: {
   //   street:string,
   //   city:string,
@@ -16,39 +17,69 @@ export class UserComponent implements OnInit {
   // }
   address: Address;
   hobbies: string[];
-  hello:any;
+  hello: any;
+  posts: Post[];
+  isEdit: boolean=false;
 
-  constructor() { 
-  console.log('constructor ran...');
+  constructor(private dataService: DataService) {
+    console.log('constructor ran...');
   }
 
   ngOnInit() {
-  
-  console.log('ngOnInit ran...');
-  this.name = 'John Doe';
-  this.age = 30;
-  this.address = {
-    street: '50 main st',
-    city: 'boston',
-    state: 'MA'
-  }
-  this.hobbies=['watch movies', 'music'];
+
+    console.log('ngOnInit ran...');
+    this.name = 'John Doe';
+    this.email = 'tett@asdf.com',
+    this.age = 30;
+    this.address = {
+      street: '50 main st',
+      city: 'boston',
+      state: 'MA'
+    }
+    this.hobbies = ['watch movies', 'music'];
+    this.hello = 'hello';
+
+    this.dataService.getPosts().subscribe((posts) => {
+      // console.log(posts);
+      this.posts = posts;
+    });
   }
 
-  onClick(){
-    this.name='Brad Traversy II';
+  onClick() {
+    this.name = 'Brad Traversy II';
     this.hobbies.push('new hobby');
   }
 
-  addHobby(hobby){
+  addHobby(hobby) {
     console.log(hobby);
     this.hobbies.unshift(hobby);
+    // push to the top
     return false;
+  }
+
+  deleteHobby(hobby) {
+    // console.log(hobby);
+    for (let i = 0; i < this.hobbies.length; i++) {
+      if (this.hobbies[i] == hobby) {
+        this.hobbies.splice(i, 1);
+      }
+    }
+  }
+
+  toggleEdit() {
+    this.isEdit = !this.isEdit;
   }
 }
 
 interface Address {
-  street:string,
-  city:string,
-  state:string
+  street: string,
+  city: string,
+  state: string
+}
+
+interface Post {
+  id: number,
+  title: string,
+  body: string,
+  userId: number
 }
